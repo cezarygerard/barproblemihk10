d8d96945a95c6d3a61405e6304a1ccecec258920
czarek.zawadka <czarek.zawadka@a2e2b3aa-0c13-bf4c-a63f-291b5ee2eaf3>
2010-12-03 20:09:51 +0000

    lots of common stuff

---
+++ src/Common.h
@@ -0,0 +1,40 @@
+/*
+ * common.h
+ *
+ *  Created on: Dec 3, 2010
+ *      Author: czarek
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <assert.h>
+//constant variables:
+const int numberOfTables;
+const int numberOfSpacecOnTable;
+const int numberOfGlasses;
+const int numberOfCups;
+const int numberOfDrinksPerCustomer;
+const double beerRatio;
+const double capuchinnoRatio;
+const double chocolateRatio;
+const int timeUntilClose;
+const int timeUntilLastCall;
+//preparing delays:
+//
+
+
+enum OrderType {beer, cappuccino, chocolate } ;
+int msqid;
+pthread_mutex_t beerTap;// = PTHREADINITIALIZE;
+pthread_mutex_t cupBoard; //??
+pthread_mutex_t milk;
+pthread_mutex_t coffe;
+pthread_mutex_t chocolate;
+pthread_mutex_t beerTap;
+
+sem_t glasses;
+sem_t tables[numberOfTables];
+
+
+#endif /* COMMON_H_ */
