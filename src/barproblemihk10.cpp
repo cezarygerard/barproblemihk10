2fe1d03c4f7c800bb5776566d745dc64e674d5f8
jonmann3000 <jonmann3000@a2e2b3aa-0c13-bf4c-a63f-291b5ee2eaf3>
2010-12-03 17:33:06 +0000

    added threads to hello world

--- src/barproblemihk10.cpp
+++ src/barproblemihk10.cpp
@@ -6,13 +6,45 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-#include <pthread.h>
 #include <semaphore.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-using namespace std;
+#define NUM_THREADS     5
 
-int main() {
-	cout << "Hello World!" << endl; // prints
-	return 0;
+void *TaskCode(void *argument)
+{
+   int tid;
+
+   tid = *((int *) argument);
+   printf("Hello World! It's me, thread %d!\n", tid);
+
+   /* optionally: insert more useful stuff here */
+
+   return NULL;
+}
+
+int main (int argc, char *argv[])
+{
+   pthread_t threads[NUM_THREADS];
+   int thread_args[NUM_THREADS];
+   int rc, i;
+
+   /* create all threads */
+   for (i=0; i<NUM_THREADS; ++i) {
+      thread_args[i] = i;
+      printf("In main: creating thread %d\n", i);
+      rc = pthread_create(&threads[i], NULL, TaskCode, (void *) &thread_args[i]);
+      assert(0 == rc);
+   }
+
+   /* wait for all threads to complete */
+   for (i=0; i<NUM_THREADS; ++i) {
+      rc = pthread_join(threads[i], NULL);
+      assert(0 == rc);
+   }
+
+   exit(EXIT_SUCCESS);
 }
