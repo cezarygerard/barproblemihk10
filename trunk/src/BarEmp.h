1665b6330385f3a459eb1f7d1a32a8994852ec7e
jonmann3000 <jonmann3000@a2e2b3aa-0c13-bf4c-a63f-291b5ee2eaf3>
2010-12-13 00:17:15 +0000

    added BarEmp class which extends person and contains the bartending functions, it is extended by landlord and barmaid

---
+++ trunk/src/BarEmp.h
@@ -0,0 +1,20 @@
+/*
+ * BarEmp.h
+ *
+ *  Created on: Dec 12, 2010
+ *      Author: jonathan
+ */
+
+#ifndef BAREMP_H_
+#define BAREMP_H_
+
+#include "Person.h"
+#include "Common.h"
+
+class BarEmp: public Person {
+public:
+	void bartend();
+
+};
+
+#endif /* BAREMP_H_ */
